cb7763abb4aa780f625df2e945f73936aa6257da
cashzain763-sys <cashzain763@gmail.com>
2026-04-17 19:14:56 +0300

    Update Main.jsx

--- src/screen/Main.jsx
+++ src/screen/Main.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import io from "socket.io-client";
 
 // export const serverRoute = "http://localhost:8080";
-export const serverRoute = "https://ssl-se1.onrender.com";
+export const serverRoute = "https://slm2-se.onrender.com";
 export const socket = io(serverRoute);
 
 const Main = () => {
